9d71a787489234375a0965cd326015ffa8084928
LNS <97752817+LeeNamsaeg@users.noreply.github.com>
2023-05-26 20:10:20 +0900

    Create StackWithFixedSizeArr.cpp

---
+++ Stack/StackWithFixedSizeArr.cpp
@@ -0,0 +1,44 @@
+#define INFINITY 2000000000
+
+#define MAX_STACK_SIZE 100
+
+using namespace std;
+
+template <typename T>
+class Stack{
+private:	
+	T datas[MAX_STACK_SIZE];
+	
+	int top;
+	
+public:
+	Stack(){
+		top = -1;
+	}
+	
+	int Push(T data){
+		if (IsFull() == true)		
+			return INFINITY;
+		
+		datas[++top] = data;
+	}
+	
+	T Pop(){
+		if (IsEmpty() == true)
+			return INFINITY;
+		
+		return (datas[top--]);
+	}
+	
+	bool IsEmpty(){
+		return (top == -1);
+	}
+	
+	bool IsFull(){
+		return (top == MAX_STACK_SIZE - 1);
+	}
+	
+	void Clear(){
+		top = -1;
+	}
+};
